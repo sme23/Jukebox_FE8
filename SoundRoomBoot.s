0338e7a915401676207bc365191faccf5fa628d3
Sme <esahig15789@gmail.com>
2021-04-07 16:25:13 -0400

    marginal progress

---
+++ SoundRoomBoot.s
@@ -0,0 +1,33 @@
+.thumb
+.align
+
+.global SoundRoomBoot
+.type SoundRoomBoot, %function
+
+.equ ProcStart,0x8002C7D
+.macro blh to, reg=r3
+  push {\reg}
+  ldr \reg, =\to
+  mov lr, \reg
+  pop {\reg}
+  .short 0xf800
+.endm
+
+SoundRoomBoot:
+
+push {r5-r7,r14}
+sub sp,#4
+
+//make a dummy proc
+ldr r0,=DummyProc
+blh ProcStart
+
+mov r4,r0
+mov r0,#0
+
+ldr r1,=0x80AF535
+bx r1
+
+.ltorg
+.align
+

16f922ba91b6d430d7550a4c711cb17eaa7c2ffd
Sme <esahig15789@gmail.com>
2021-04-29 02:29:04 -0400

    its kind of working???
    sprites are still all absent but you can mechanically work the sound room now

--- SoundRoomBoot.s
+++ SoundRoomBoot.s
@@ -4,13 +4,8 @@
 .global SoundRoomBoot
 .type SoundRoomBoot, %function
 
-.equ ProcStart,0x8002C7D @r0 = proc address in ROM to start, r1 = parent proc address in RAM
-.equ ProcStartBlocking, 0x8002ce1 @r0 = proc address in ROM to start, r1 = parent proc address in RAM
-.equ RegisterTileGraphics, 0x08002015 @r0 = frame data????, r1 = area of vram, r2 = 0x2000???? (i dont think this is actually necessary?)
-.equ LoadBgConfig, 0x0881B59 @r0 = something but we can keep it as 0
-.equ InitOAMSplice,0x80020fd
-.equ SoundRoomStart, 0x80AFF1C
-
+.equ ProcStart,0x8002C7D
+.equ ProcStartBlocking,0x08002CE1
 .macro blh to, reg=r3
   push {\reg}
   ldr \reg, =\to
@@ -18,25 +13,6 @@
   pop {\reg}
   .short 0xf800
 .endm
-
-.global SoundRoomBootFunc
-.type SoundRoomBootFunc, %function
-
-SoundRoomBootFunc:
-push {r14}
-//make a dummy proc
-ldr r0,=DummyProc
-mov r1,#0
-mov r2,#0
-mov r3,#0
-mov r5,#0
-blh ProcStart
-pop {r0}
-bx r0
-
-.ltorg
-.align
-
 
 SoundRoomBoot:
 
@@ -48,16 +24,9 @@ ldr r0,=DummyProc
 mov r1,#0
 mov r2,#0
 mov r3,#0
-mov r5,#0
 blh ProcStart
 
 mov r4,r0
-
-//mov r1,r4
-//ldr r0,=#0x8A21338 //sound room UI proc
-//blh ProcStart
-
-
 mov r0,#0
 
 ldr r1,=0x80AF535
@@ -66,34 +35,53 @@ bx r1
 .ltorg
 .align
 
-<<<<<<< HEAD
-.global InfiniteLoopProcFunc
-.type InfiniteLoopProcFunc, %function
 
-InfiniteLoopProcFunc:
+.global InfiniteLoopProcRoutine
+.type InfiniteLoopProcRoutine, %function
+
+InfiniteLoopProcRoutine:
 bx r14
 
 .ltorg
 .align
 
-.global Frame1BootFunc
-.type Frame1BootFunc, %function
 
-Frame1BootFunc:
-//this used to be dumb but now it's going to actually do something
-push {r14}
+.global BeginningProcStarterThings
+.type BeginningProcStarterThings, %function
+
+BeginningProcStarterThings:
+
+push {r4,r14}
+mov r4,r0 @r4 = parent proc ptr
+
+ldr r0,=#0x08A21338 @SoundRoomUI proc
+mov r1,r4
+mov r2,#0
+mov r3,#0
+blh ProcStartBlocking
+
+pop {r4}
 pop {r0}
 bx r0
 
 .ltorg
 .align
 
-.global GrrThisIsWhatIGet
-.type GrrThisIsWhatIGet, %function
+.global NewBootFunc
+.type NewBootFunc, %function
 
-GrrThisIsWhatIGet:
-mov r0,#0
-mov r1,#0xF
-blh InitOAMSplice
+NewBootFunc:
+push {r14}
 
+//make a dummy proc
+ldr r0,=DummyProc
+mov r1,#0
+mov r2,#0
+mov r3,#0
+blh ProcStart
 
+pop {r0}
+bx r0
+
+.ltorg
+.align
